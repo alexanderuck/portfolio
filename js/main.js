a859194291dc98d7f2fa767230a28e47e6cd7f91
alexanderuck <80569120+alexanderuck@users.noreply.github.com>
2021-11-01 12:09:31 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(window).on('load', function () {
+    // Vide.js - video bg
+    $('#header').vide('./video/cover', {
+        bgColor: '#242123'
+        // bgColor: '#141413'
+    })
+})
+
+
+// Плавная прокрутка 
+
+$(document).ready(function () {
+    $("#header").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 1000);
+
+    });
+});
+
+
+
+// document.querySelector('.portfolio').classList.remove('hidden');
+
+// window.onload = () => document.querySelector('.portfolio').classList.remove('hidden');
+
+// document.querySelector('portfolio').classList.remove('hidden');
+
+// window.onload = () => document.querySelector('header').classList.remove('hidden');
+// .hidden {
+//     opacity: 0;
+// }
+
+// header {
+//     transition: all 2s ease;
+// }
